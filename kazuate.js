0386c2b411762228819c0a368f77117be3ef9f58
G284082022 <228408@st.takushoku-u.ac.jp>
2023-05-25 13:57:23 +0900

    第5回資料

--- kazuate.js
+++ kazuate.js
@@ -18,12 +18,10 @@ hantei();
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
-  console.log((kaisu)+"回目の予想: "+(yoso))
+  
 
-  let span = document.querySelector('span#kaisu');		
-  let p = document.createElement('p');				
-  p.textContent = (kaisu)+'回目の予想: '+(yoso);						
-  span.insertAdjacentElement('afterend', p);
+  let p2 = document.querySelector('span#kaisu');		
+  p2.textContent = (kaisu)+'回目の予想: '+(yoso);	
   
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
