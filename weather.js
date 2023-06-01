64d0425f1670da2bda4a1f06859a3ff8a89c015b
G284082022 <228408@st.takushoku-u.ac.jp>
2023-06-01 17:16:55 +0900

    プロジェクト課題

--- weather.js
+++ weather.js
@@ -45,24 +45,111 @@ let data = {
   "cod": 200
 };
 
+let mydata = [
+  {tosimei: "カイロ",tosiID: "360630"},
+  {tosimei: "モスクワ",tosiID: "524901"},
+  {tosimei: "ヨハネスブルク",tosiID: "993800"},
+  {tosimei: "北京",tosiID: "1816670"},
+  {tosimei: "東京",tosiID: "1850147"},
+  {tosimei: "シンガポール",tosiID: "1880252"},
+  {tosimei: "シドニー",tosiID: "2147714"},
+  {tosimei: "ロンドン",tosiID: "2643743"},
+  {tosimei: "パリ",tosiID: "2968815"},
+  {tosimei: "リオデジャネイロ",tosiID: "3451189"},
+  {tosimei: "ニューヨーク",tosiID: "5128581"},
+  {tosimei: "ロサンゼルス",tosiID: "5368361"}
+]
+
 ////////// 課題3-2 ここからプログラムを書こう
-console.log(data.name)
-console.log(data.main.temp_min)
-console.log(data.main.temp_max)
-const para = document.querySelector('p');
-
-let weather = document.querySelector('span#tenki');		
-weather.textContent = (data.weather.description);	
-let max = document.querySelector('span#saikoukion');		
-max.textContent = (data.main.temp_max);	
-let min = document.querySelector('span#saiteikion');		
-min.textContent = (data.main.temp_min);	
-
-
-
 let kensaku = document.querySelector('button#kensaku');
 kensaku.addEventListener('click', where);
 
-function where(){
+
+
+// 通信を開始する処理
+function where() {
+
+    let i = document.querySelector('input[name="tosi"]');
+    let tosi = i.value;       // ユーザが記入した文字列
+    console.log(tosi);
+    for(let n of mydata){
+      if(tosi==(n.tosimei)){
+        //検索した文字を数字に変換し、その数字をidに代入する
+        let id =(n.tosiID);
+  
+        // URL を設定
+        let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+id+'.json';
+  
+        // 通信開始
+        axios.get(url)
+         .then(showResult)   // 通信成功
+         .catch(showError)   // 通信失敗
+         .then(finish);      // 通信の最後の処理
+      }
+    }
+    
+    if(tosi=='カイロ'){
+      //検索した文字を数字に変換し、その数字をidに代入する
+      let id ='360630';
+
+      // URL を設定
+      let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+id+'.json';
+      
+      // 通信開始
+      axios.get(url)
+       .then(showResult)   // 通信成功
+       .catch(showError)   // 通信失敗
+       .then(finish);      // 通信の最後の処理
+    }   
 }
+
+// 通信が成功した時の処理
+function showResult(resp) {
+    // サーバから送られてきたデータを出力
+    let data = resp.data;
+
+    // data が文字列型なら，オブジェクトに変換する
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+
+    // data をコンソールに出力
+    console.log(data);
+
+    console.log(data.name)
+    console.log(data.weather[0].description)
+    console.log(data.main.temp_min)
+    console.log(data.main.temp_max)
+    console.log(data.main.humidity)
+    const para = document.querySelector('p');
+
+    // data.x を出力
+    console.log(data.x);
+
+    let tenkiyohou = document.querySelector('span#tenki');		
+    tenkiyohou.textContent = (data.weather[0].description);	
+    let max = document.querySelector('span#saikoukion');		
+    max.textContent = (data.main.temp_max);	
+    let min = document.querySelector('span#saiteikion');		
+    min.textContent = (data.main.temp_min);	
+    let situdo = document.querySelector('span#situdo');		
+    situdo.textContent = (data.main.humidity);	
+
+}
+
+// 通信エラーが発生した時の処理
+function showError(err) {
+    console.log(err);
+}
+
+// 通信の最後にいつも実行する処理
+function finish() {
+    console.log('Ajax 通信が終わりました');
+}
+
+
+
+
+
+
 
